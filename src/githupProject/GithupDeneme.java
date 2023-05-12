651a5956d2db8cb9c50c3fc0c378cf846c56f8ce
Leyla Simitci <leylacayli@hotmail.com>
2023-05-12 11:04:37 +0300

    L

---
+++ src/githupProject/GithupDeneme.java
@@ -0,0 +1,25 @@
+package githupProject;
+
+public class GithupDeneme {
+
+
+//kullanicidan bir karakter girmesini isteyin ve girilen karakterin harf olup olmadigini kontrol edin
+
+        /*Scanner input = new Scanner(System.in);
+        System.out.println("lutfen bir karakter giriniz");
+        String str=input.next().toUpperCase();
+        int strInt =str.charAt(0);
+        if(strInt>=65 && strInt<=90){
+            System.out.println("girilen karakter bir harftir");
+        }else System.out.println("girilen karakter bir harf degildir");*/
+
+        /*Kullanıcıdan bir karakter sisteme girmesini isteyin.
+        Ternary kullanarak;
+        bu karakterin bir harf olup olmadıgını kontrol edin.
+        Harf ise büyük veya kucuk harf oldugunu konsola yazdırın
+        Harf degilse konsola "Harf Degil" yazdırın
+                */
+
+
+
+}
